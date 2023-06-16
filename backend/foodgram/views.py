932be149345a1ebf629b26757b9b2c49170a7497
Nikita <Nylesszpg@gmail.com>
2023-06-17 00:49:32 +0300

    review v5

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import DjangoFilterBackend
+# from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework import viewsets, status
