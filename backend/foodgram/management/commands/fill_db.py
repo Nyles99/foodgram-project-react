b71ddb458d6654e89b03638d9b0be9bc46a5cd56
Nikita <Nylesszpg@gmail.com>
2023-06-24 23:01:09 +0300

    finish v6

--- backend/foodgram/management/commands/fill_db.py
+++ backend/foodgram/management/commands/fill_db.py
@@ -8,10 +8,10 @@ from foodgram import models
 class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('foodgram/management/commands/ingredients.csv',
-            encoding='utf-8') as fixture:
+                   encoding='utf-8') as fixture:
             reader = csv.reader(fixture)
             for line in reader:
-                name, measurement_unit = line 
+                name, measurement_unit = line
                 models.Ingredient.objects.get_or_create(
                     name=name, measurement_unit=measurement_unit
                 )
