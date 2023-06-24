408bfd7480698b4825cb644e2fc61c009c254df2
Nikita <Nylesszpg@gmail.com>
2023-06-24 23:19:11 +0300

    finish v10

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -19,10 +19,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('id', models.AutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
+                ('password', models.CharField(
+                    max_length=128,
+                    verbose_name='password'
+                )),
+                ('last_login', models.DateTimeField(
+                    blank=True,
+                    null=True,
+                    verbose_name='last login'
+                )),
+                ('is_superuser', models.BooleanField(
+                    default=False,
+                    help_text='Designates that this user has all'
+                              'permissions without explicitly assigning them.',
+                    verbose_name='superuser status'
+                )),
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
