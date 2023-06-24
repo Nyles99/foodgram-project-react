9129f83a90bcc87468ee9ff9468a5681b46acfdc
Nikita <Nylesszpg@gmail.com>
2023-06-24 22:59:22 +0300

    finish v5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,19 +104,19 @@ DJOSER = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": ("django.contrib.auth.password_validation"
-                ".UserAttributeSimilarityValidator"),
+                 ".UserAttributeSimilarityValidator"),
     },
     {
         "NAME": ("django.contrib.auth.password_validation"
-                ".MinimumLengthValidator"),
+                 ".MinimumLengthValidator"),
     },
     {
         "NAME": ("django.contrib.auth.password_validation"
-                ".CommonPasswordValidator"),
+                 ".CommonPasswordValidator"),
     },
     {
         "NAME": ("django.contrib.auth.password_validation"
-                ".NumericPasswordValidator"),
+                 ".NumericPasswordValidator"),
     },
 ]
 
