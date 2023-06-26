ed38d935d87d20695c3e53eb57fdc7a3435c30d4
Nikita <Nylesszpg@gmail.com>
2023-06-26 23:23:14 +0300

    finalo4ka v22

--- backend/foodgram/filters.py
+++ backend/foodgram/filters.py
@@ -16,7 +16,7 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ["author", "tags", "is_favorited", "is_in_shopping_cart"]
+        fields = ("author", "tags", "is_favorited", "is_in_shopping_cart")
 
     def get_is_favorited(self, queryset, name, value):
         user = self.request.user
