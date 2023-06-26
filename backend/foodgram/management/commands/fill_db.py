2c7f765c022b4ef1ac33ac1aeccb9e6cd7e3e907
Никита Молчанов <nylesszpg@gmail.com>
2023-06-26 12:43:40 +0300

    finalo4ka v8

--- backend/foodgram/management/commands/fill_db.py
+++ backend/foodgram/management/commands/fill_db.py
@@ -2,16 +2,18 @@ import csv
 
 from django.core.management.base import BaseCommand
 
-from foodgram import models
+from foodgram.models import Ingredient
 
 
 class Command(BaseCommand):
+    help = "Load ingredients to DB"
+
     def handle(self, *args, **options):
         with open('foodgram/management/commands/ingredients.csv',
                   encoding='utf-8') as fixture:
             reader = csv.reader(fixture)
             for line in reader:
                 name, measurement_unit = line
-                models.Ingredient.objects.get_or_create(
+                Ingredient.objects.get_or_create(
                     name=name, measurement_unit=measurement_unit
                 )
