4e0e309353d8fb5e242ad2a12a7920f9a1b26176
Nikita <Nylesszpg@gmail.com>
2023-06-28 00:36:11 +0300

    new v9

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,9 +5,10 @@ from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet
 
 router = DefaultRouter()
-router.register("users", CustomUserViewSet)
+router.register("users", CustomUserViewSet, basename='users')
 
 urlpatterns = [
-    path("auth/", include("djoser.urls.authtoken")),
-    path("", include(router.urls)),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
