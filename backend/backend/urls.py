ec58e5ad8a1683bfbd207aff9010f4e7ab1f6978
Никита Молчанов <nylesszpg@gmail.com>
2023-06-28 13:03:21 +0300

    new v31

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import include
 from django.urls import path
 from django.views.generic import TemplateView
 
+from backend.foodgram import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/docs/", TemplateView.as_view(template_name="redoc.html")),
     path("api/", include("users.urls")),
     path("api/", include("foodgram.urls")),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
