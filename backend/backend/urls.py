83da0dee33518fffb91dc421e8fa6342913dd346
Никита Молчанов <nylesszpg@gmail.com>
2023-06-28 13:39:31 +0300

    new v36

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
