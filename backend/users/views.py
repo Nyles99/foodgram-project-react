82d1340c07964cd215fc575a5437b63ce8a23d04
Никита Молчанов <nylesszpg@gmail.com>
2023-06-29 09:59:45 +0300

    new v57

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.hashers import make_password
 from djoser.views import UserViewSet
-from rest_framework import status, permissions
+from rest_framework import status, exceptions
 from rest_framework.permissions import (
     IsAuthenticated,
 )
@@ -13,10 +13,8 @@ from rest_framework.response import Response
 from users.pagination import CustomPaginator
 from .serializers import (
     CustomUserSerializer,
-    PasswordSerializer,
     ShowFollowerSerializer,
-    FollowerSerializer,
-    UserCreateSerializer,
+    FollowerSerializer
 )
 from .models import Follow
 
@@ -26,69 +24,48 @@ User = get_user_model()
 
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    serializer_class = CustomUserSerializer
     pagination_class = CustomPaginator
-
-    def get_serializer_class(self):
-        if self.action == 'set_password':
-            return PasswordSerializer
-        if self.action == 'create':
-            return UserCreateSerializer
-        return CustomUserSerializer
 
     @action(
         detail=False,
         methods=['GET'],
-        permission_classes=(permissions.IsAuthenticated,))
-    def me(self, request, *args, **kwargs):
-        user = get_object_or_404(User, pk=request.user.id)
-        serializer = CustomUserSerializer(user)
-        return Response(serializer.data)
-
-    def perform_create(self, serializer):
-        if "password" in self.request.data:
-            password = make_password(self.request.data["password"])
-            serializer.save(password=password)
-        else:
-            serializer.save()
-
-    @action(["post"], detail=False)
-    def set_password(self, request, *args, **kwargs):
-        user = self.request.user
-        serializer = PasswordSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
-        user.set_password(serializer.validated_data["new_password"])
-        user.save()
-        return Response({"status": "password set"})
+        permission_classes=[IsAuthenticated],
+        serializer_class=FollowerSerializer
+    )
+    def subscriptions(self, request):
+        user = request.user
+        favorites = user.followers.all()
+        users = User.objects.filter(id__in=[f.author.id for f in favorites])
+        paginated_queryset = self.paginate_queryset(users)
+        serializer = self.serializer_class(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
     @action(
-        methods=["get", "delete", "post"],
         detail=True,
-        permission_classes=[IsAuthenticated],
+        methods=('post', 'delete'),
+        serializer_class=FollowerSerializer
     )
-    def subscribe(self, request, pk=None):
+    def subscribe(self, request, id=None):
         user = request.user
-        author = get_object_or_404(User, pk=pk)
-        follow = Follow.objects.filter(user=user, author=author)
-        data = {
-            "user": user.id,
-            "author": author.id,
-        }
-        if request.method == "GET" or request.method == "POST":
-            if follow.exists():
-                return Response(
-                    "Вы уже подписаны", status=status.HTTP_400_BAD_REQUEST
-                )
-            serializer = FollowerSerializer(data=data, context=request)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
+        author = get_object_or_404(User, pk=id)
+        if request.method == 'POST':
+            if user == author:
+                raise exceptions.ValidationError(
+                    'Подписываться на себя запрещено.')
+            if Follow.objects.filter(user=user, author=author).exists():
+                raise exceptions.ValidationError(
+                    'Вы уже подписаны на этого пользователя.')
+            Follow.objects.create(user=user, author=author)
+            serializer = self.get_serializer(author)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        elif request.method == "DELETE":
-            follow.delete()
-            return Response("Удалено", status=status.HTTP_204_NO_CONTENT)
+
+        if request.method == 'DELETE':
+            if not Follow.objects.filter(user=user, author=author).exists():
+                raise exceptions.ValidationError(
+                    'Вы не подписаны на этого пользователя.')
+            Follow.objects.filter(user=user, author=author).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(
         methods=["get", "post"],
