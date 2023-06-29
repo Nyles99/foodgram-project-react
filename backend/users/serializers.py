db045515d5bad251c4545ba5fc91b49388618d04
Никита Молчанов <nylesszpg@gmail.com>
2023-06-29 09:20:36 +0300

    new v53

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -24,6 +24,27 @@ class CustomUserSerializer(UserSerializer):
             return False
         return Follow.objects.filter(user=request.user, author=obj.id).exists()
 
+
+class UserCreateSerializer(UserCreateSerializer):
+
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'password')
+        required_fields = (
+            'id', 'email', 'username', 'first_name', 'last_name', 'password')
+        validators = [UniqueTogetherValidator(
+            queryset=User.objects.all(),
+            fields=('username', 'email')
+        )]
+
+    def validate(self, data):
+        if not re.match(r'^[\w.@+-]+', str(data.get('username'))):
+            raise serializers.ValidationError(
+                'Неверный формат имени.'
+            )
+        return data
+
     def validate_email(email):
         email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         return re.match(email_regex, email)
@@ -55,27 +76,6 @@ class CustomUserSerializer(UserSerializer):
                 f'Имя пользователя {username} недопустимо. '
                 'Используйте другое имя.')
         return username
-
-
-class UserCreateSerializer(UserCreateSerializer):
-
-    class Meta(UserCreateSerializer.Meta):
-        model = User
-        fields = ('id', 'email', 'username', 'first_name', 'last_name',
-                  'password')
-        required_fields = (
-            'id', 'email', 'username', 'first_name', 'last_name', 'password')
-        validators = [UniqueTogetherValidator(
-            queryset=User.objects.all(),
-            fields=('username', 'email')
-        )]
-
-    def validate(self, data):
-        if not re.match(r'^[\w.@+-]+', str(data.get('username'))):
-            raise serializers.ValidationError(
-                'Неверный формат имени.'
-            )
-        return data
 
 
 class PasswordSerializer(serializers.Serializer):
