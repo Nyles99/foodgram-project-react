bbfc6ff6a63e447dd49c3f1881551f724cc3035a
Никита Молчанов <nylesszpg@gmail.com>
2023-07-03 13:28:51 +0300

    new v101

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -55,11 +55,6 @@ class Ingredient(models.Model):
     class Meta:
         verbose_name = "Ингредиент"
         verbose_name_plural = "Ингредиенты"
-        constraints = [
-            models.UniqueConstraint(
-                fields=('name', 'measurement_unit'),
-                name='unique_ingredient'),
-        ]
 
     def __str__(self):
         return self.name
