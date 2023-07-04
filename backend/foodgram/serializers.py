e56dd94e0efb84c8f22a504b213ab2585a1e6e73
Nikita <Nylesszpg@gmail.com>
2023-07-04 20:55:50 +0300

    new v123

--- backend/foodgram/serializers.py
+++ backend/foodgram/serializers.py
@@ -5,6 +5,7 @@ from drf_extra_fields.fields import Base64ImageField
 from foodgram.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             ShoppingCart, Tag)
 from rest_framework import exceptions, serializers
+from users.serializers import CustomUserSerializer
 
 User = get_user_model()
 
