16b705627322ab93157f7948c65c75e058b39a66
Никита Молчанов <nylesszpg@gmail.com>
2023-07-05 12:23:37 +0300

    newv144

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import status, viewsets
-from rest_framework.decorators import action, exceptions
+from rest_framework import status, viewsets, exceptions
+from rest_framework.decorators import action
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         SAFE_METHODS)
 from rest_framework.response import Response
