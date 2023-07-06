717606de8fdadbecca23da66a0926de320323e4a
Никита Молчанов <nylesszpg@gmail.com>
2023-07-06 13:05:41 +0300

    new v163

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+app_name = 'api'
+
 from .views import (
     IngredientViewSet,
     RecipeViewSet,
