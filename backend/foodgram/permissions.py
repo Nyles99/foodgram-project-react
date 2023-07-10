b11147d66e66252a4dfa83ad362408e9c1cc3fde
Никита Молчанов <nylesszpg@gmail.com>
2023-07-10 11:08:51 +0300

    new v229

--- backend/foodgram/permissions.py
+++ backend/foodgram/permissions.py
@@ -1,18 +1,16 @@
 from rest_framework import permissions
 
 
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
+
+
 class IsAdminOrReadOnly(permissions.BasePermission):
-    '''Доступ имеет только админ'''
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_staff)
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    '''Доступ имеет только автор'''
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                and obj.author == request.user)
